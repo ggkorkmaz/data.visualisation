d616e1b0622660017bb0b802a08b428aa41522fe
ggkorkmaz <141242463+ggkorkmaz@users.noreply.github.com>
2023-08-03 11:55:17 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -10,8 +10,6 @@ for i in os.listdir():
   if i.endswith('.csv'): 
     file_name_list.append(i)
   
-st.write('Hello world')
-
 df=pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
@@ -24,8 +22,8 @@ el_y =st.selectbox('select y element', el_list)
 file_name=st.selectbox('select file', file_name_list)
 
 df=pd.read_csv('file_name')
-st.dataframe(df)
-p = figure(
-    title='simple line example',
-    x_axis_label='x',
-    y_axis_label='y')
+p = figure(x_axis_label='x', y_axis_label='y')
+
+p.circle(df[el_x],df[el_y]
+
+st.bokeh_chart(p)
