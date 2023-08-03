3046015147d6997d2efe332bf3fce8a4d831f0de
ggkorkmaz <141242463+ggkorkmaz@users.noreply.github.com>
2023-08-03 10:51:07 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,3 +1,5 @@
+from bokeh.plotting import figure,show
+
 import streamlit as st
 import pandas as pd 
 
@@ -13,22 +15,17 @@ st.write('Hello world')
 df=pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
-el_list=df.columns.tolist()[27:80]
-x_axis=st.selectbox('select element', el_list)
+el_list=df.columns.tolist()[27:100]
+#x_axis=st.selectbox('select element', el_list)
 
-st.multiselect('select location', file_name_list)
+el_x =st.selectbox('select x element', el_list)
+el_y =st.selectbox('select y element', el_list)
 
-import streamlit as st
-from bokeh.plotting import figure
+file_name=st.selectbox('select file', file_name_list)
 
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
-
+df=pd.read_csv('file_name')
+st.dataframe(df)
 p = figure(
     title='simple line example',
     x_axis_label='x',
     y_axis_label='y')
-
-p.line(x, y, legend_label='Trend', line_width=2)
-
-st.bokeh_chart(p, use_container_width=True) 
