4d95c3c8e8ae1ce7bdefa5f9f228fb4cbd1efdce
ggkorkmaz <141242463+ggkorkmaz@users.noreply.github.com>
2023-08-03 12:05:16 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -23,6 +23,6 @@ file_name=st.selectbox('select file', file_name_list)
 df=pd.read_csv('file_name')
 p = figure(x_axis_label='x', y_axis_label='y')
 
-p.circle(df[el_x],df[el_y])
+p.circle(df[el_x], df[el_y])
 
 st.bokeh_chart(p)
