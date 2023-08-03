30c6370faee1d7cbf059b35e15b303303f679fa0
ggkorkmaz <141242463+ggkorkmaz@users.noreply.github.com>
2023-08-03 13:07:17 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -26,8 +26,5 @@ p = figure(x_axis_label='X', y_axis_label='Y') #title
 p.circle(df[el_x], df[el_y]) #color, size, alpha
 p.line([np.min(df[el_x]), np.max(df[el_y])], ([np.mean(df[el_x]), np.mean(df[el_y])]))
                                                                    
-#p.line([0,20], [5,5]) #line_width=2, line_color='green'
-#p.rect(x=24, y=4, height=3, color='yellow')
-
 st.header("plot")
 st.bokeh_chart(p)
