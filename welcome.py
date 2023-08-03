c830c8d91013258c76012980393d59a585aecd9e
ggkorkmaz <141242463+ggkorkmaz@users.noreply.github.com>
2023-08-03 11:58:49 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -2,7 +2,6 @@ from bokeh.plotting import figure,show
 
 import streamlit as st
 import pandas as pd 
-
 import os
 
 file_name_list=[]
@@ -24,6 +23,6 @@ file_name=st.selectbox('select file', file_name_list)
 df=pd.read_csv('file_name')
 p = figure(x_axis_label='x', y_axis_label='y')
 
-p.circle(df[el_x],df[el_y]
+p.circle(df[el_x],df[el_y])
 
 st.bokeh_chart(p)
