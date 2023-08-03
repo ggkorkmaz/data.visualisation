5eed181047b112e652159f1257b5ae089763de30
ggkorkmaz <141242463+ggkorkmaz@users.noreply.github.com>
2023-08-03 12:12:04 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -13,14 +13,14 @@ df=pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
 el_list=df.columns.tolist()[27:100]
-#x_axis=st.selectbox('select element', el_list)
+
 
 el_x =st.selectbox('select x element', el_list)
 el_y =st.selectbox('select y element', el_list)
 
 file_name=st.selectbox('select file', file_name_list)
 
-#df=pd.read_csv('file_name')
+
 p = figure(x_axis_label='X', y_axis_label='Y')
 
 p.circle(df[el_x], df[el_y])
